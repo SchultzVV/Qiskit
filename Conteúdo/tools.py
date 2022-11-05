1dfb4b16d94c9384582356bff436bf7752b91b43
v <vitor.schultz@marinhocorp.com.br>
2022-11-04 21:44:59 -0300

    adicionando vqa para estado usando o rdmg_ginibre para gerar os estados aleatórios e o valor esperado como parâmetro treinável.

--- Conteúdo/tools.py
+++ Conteúdo/tools.py
@@ -1,7 +1,8 @@
 from qiskit.quantum_info import Statevector
 from qiskit.visualization import plot_bloch_multivector
 from sympy import conjugate
-
+from torch.autograd import Variable
+import torch
 
 def bloch_sphere(circuit):
     state = Statevector(circuit)
@@ -12,3 +13,10 @@ def inner_product(v,w):
     for j in range(0,d):
         ip += conjugate(v[j])*w[j]
     return ip
+
+def gen_paulis():
+    Paulis = Variable(torch.zeros([3, 2, 2], dtype=torch.complex128), requires_grad=False)
+    Paulis[0] = torch.tensor([[0, 1], [1, 0]])        
+    Paulis[2] = torch.tensor([[1, 0], [0, -1]])
+    Paulis[1] = torch.tensor([[0, -1j], [1j, 0]])
+    return Paulis
